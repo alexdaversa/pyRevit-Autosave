260e56828f409715bbc2915080c6b6bb67eb0ec1
alexdaversa <alexander.daversa@gmail.com>
2024-12-19 10:04:56 -0500

    update selfinit function to resolve error loading icons

--- pyRevit.tab/Autosave.panel/Autosave.stack/Autosave.smartbutton/script.py
+++ pyRevit.tab/Autosave.panel/Autosave.stack/Autosave.smartbutton/script.py
@@ -1,7 +1,8 @@
 from pyrevit.coreutils.ribbon import ICON_MEDIUM
-from pyrevit import script
+from pyrevit import script, forms
+from pyrevit.revit import ui
 from pyrevit.userconfig import user_config
-from pyrevit import forms
+import pyrevit.extensions as exts
 
 __context__ = "zero-doc"
 __title__ = "Autosave"
@@ -9,11 +10,13 @@ __author__ = "Alex D\'Aversa"
 
 
 def __selfinit__(script_cmp, ui_button_cmp, __rvt__):
+    on_icon = ui.resolve_icon_file(script_cmp.directory, exts.DEFAULT_ON_ICON_FILE)
+    off_icon = ui.resolve_icon_file(script_cmp.directory, exts.DEFAULT_OFF_ICON_FILE)
     if user_config.has_section("autosave") and user_config.autosave.has_option(
         "enabled"
     ):
         button_icon = script_cmp.get_bundle_file(
-            "on.png" if user_config.autosave.get_option("enabled") else "off.png"
+            on_icon if user_config.autosave.get_option("enabled") else off_icon
         )
     else:
         user_config.add_section("autosave")
@@ -21,7 +24,7 @@ def __selfinit__(script_cmp, ui_button_cmp, __rvt__):
         user_config.autosave.enabled = False
         user_config.save_changes()
         button_icon = script_cmp.get_bundle_file(
-            "on.png" if user_config.autosave.get_option("enabled") else "off.png"
+            on_icon if user_config.autosave.get_option("enabled") else off_icon
         )
     ui_button_cmp.set_icon(button_icon, icon_size=ICON_MEDIUM)
 
